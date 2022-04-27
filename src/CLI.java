faf073912266f2542fc49a5b15582e5ee8237249
Lautaro Barba <lau_mb94@hotmail.com>
2022-04-27 14:57:03 -0300

    Se creo singleton para manejar las conexiones a la db

--- src/CLI.java
+++ src/CLI.java
@@ -1,11 +1,12 @@
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import config.Database;
+
 import java.sql.SQLException;
 
 public class CLI {
     public static void main(String[] args) throws SQLException {
         System.out.println("Ejecutando CLI...");
 
-
+        ConnectionSource connection = new Database().getInstance();
     }
 }
--- src/config/Database.java
+++ src/config/Database.java
@@ -6,14 +6,33 @@ import com.j256.ormlite.support.ConnectionSource;
 import java.sql.SQLException;
 
 public class Database {
+    private static ConnectionSource connection = null;
 
     public String getDatabaseUrl() {
         return "jdbc:sqlite:db/example.db";
     }
 
-    public ConnectionSource getDatabaseConnection() throws SQLException {
-        ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
-        System.out.println("Conexión con database iniciada correctamente...");
-        return connectionSource;
+    public ConnectionSource getInstance() throws SQLException {
+        if(connection == null){
+            connection = new JdbcConnectionSource(this.getDatabaseUrl());
+            System.out.println("Conexión con database iniciada correctamente...");
+        }
+        return connection;
+    }
+
+    public boolean closeConnection(){
+        try {
+            connection.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean checkTables() {
+        //this.
+        System.out.println("Controlando tablas existentes...");
+        return true;
     }
 }
