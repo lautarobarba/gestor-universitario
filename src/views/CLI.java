4c9f3f0ec4b5833818b2ff8d9de1709e05262087
Lautaro Barba <lau_mb94@hotmail.com>
2022-07-27 19:21:28 -0300

    Se agrego cerrar sesion en el CLI

--- src/views/CLI.java
+++ src/views/CLI.java
@@ -38,7 +38,7 @@ public class CLI {
                 // Menu de usuario ADMINISTRADOR
                 System.out.println("== Ejecutando CLI ["+ user.getRole() + ": " +user.getName() +"] ==");
                 System.out.println("==  MENU  ==");
-                System.out.println("1 - Dar de alta un usuario.");
+                System.out.println("1 - Cerrar sesión.");
                 System.out.println();
                 System.out.println("0 - Salir.");
                 System.out.print(">> ");
@@ -46,7 +46,7 @@ public class CLI {
                 // Menu de usuario PROFESOR
                 System.out.println("== Ejecutando CLI ["+ user.getRole() + ": " +user.getName() +"] ==");
                 System.out.println("==  MENU  ==");
-                System.out.println("2 - Dar de alta un usuario.");
+                System.out.println("1 - Cerrar sesión.");
                 System.out.println();
                 System.out.println("0 - Salir.");
                 System.out.print(">> ");
@@ -54,7 +54,14 @@ public class CLI {
                 // Menu de usuario ALUMNO
                 System.out.println("== Ejecutando CLI ["+ user.getRole() + ": " +user.getName() +"] ==");
                 System.out.println("==  MENU  ==");
-                System.out.println("2 - Dar de alta un usuario.");
+                System.out.println("1 - Cerrar sesión.");
+                System.out.println();
+                System.out.println("0 - Salir.");
+                System.out.print(">> ");
+            } else {
+                // Menu de usuario SIN ROL
+                System.out.println("== Ejecutando CLI ["+ user.getRole() + ": " +user.getName() +"] ==");
+                System.out.println("==  ¡¡ERROR USUARIO SIN ROL!!  ==");
                 System.out.println();
                 System.out.println("0 - Salir.");
                 System.out.print(">> ");
@@ -78,6 +85,36 @@ public class CLI {
                         } else {
                             System.out.println("SESIÓN INICIADA");
                         }
+                        break;
+                    case 2: // 2 -
+                        break;
+                }
+            } else if (user.getRole().equals("ADMINISTRADOR")){
+                switch (seleccion) {
+                    case 1: // 1 - Cerrar sesión.
+                        System.out.println("= Cerrar sesión =");
+                        System.out.println("Cerrando sesión...");
+                        user = null;
+                        break;
+                    case 2: // 2 -
+                        break;
+                }
+            } else if (user.getRole().equals("PROFESOR")){
+                switch (seleccion) {
+                    case 1: // 1 - Cerrar sesión.
+                        System.out.println("= Cerrar sesión =");
+                        System.out.println("Cerrando sesión...");
+                        user = null;
+                        break;
+                    case 2: // 2 -
+                        break;
+                }
+            } else if (user.getRole().equals("ALUMNO")) {
+                switch (seleccion) {
+                    case 1: // 1 - Cerrar sesión.
+                        System.out.println("= Cerrar sesión =");
+                        System.out.println("Cerrando sesión...");
+                        user = null;
                         break;
                     case 2: // 2 -
                         break;
